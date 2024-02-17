d4731a2ac00ee9aff12d0c644b4692d90e26bc42
bimal-bp <bimalpatrap@gmail.com>
2024-02-17 16:42:29 +0530

    utils added

---
+++ src/utils/common.py
@@ -0,0 +1,73 @@
+import os 
+import sys 
+
+import pandas as pd 
+import numpy as np 
+import dill 
+import pickle
+from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.base import BaseEstimator,TransformerMixin
+from sklearn.preprocessing import LabelEncoder
+from src.excep.exception import CustomException
+
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,optimizer,loss):
+    try:
+        report ={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            # para=param[list(models.keys())[i]]
+            # gs=GridSearchCV(model,para,CV=3)
+            # gs.fit(X_train,y_train)
+
+            model.compile(optimizer=optimizer,loss=loss)
+            model.fir([X_train[:,0],X_train[:,1]],y_train,validation_split=0.2,
+            epochs=1,batch_size=48,verbose=1)
+
+            y_train_pred=model.predict([X_train[:,0],X_train[:,1]])
+            y_test_pred=model.predict([X_test[:,0],X_test[:,1]])
+
+            train_model_score=mean_squared_error(y_train,y_train_pred)
+            test_model_score=mean_squared_error(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+
+        return report 
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+class LabelEncoderTransformer(BaseEstimator,TransformerMixin):
+    def fit(self,X,y=None):
+        self.encoder={}
+        for column in X.columns:
+            if X[column].dtype=='object':
+                le=LabelEncoder()
+                le.fit(X[column])
+                self.encoder[column]=le 
+        return self
+
+    def transform(self,X):
+        X_copy=X.copy()
+        for column,encoder in self.encoders.items():
+            if column in X_copy.columns:
+                X_copy[column]=encoder.transform(X_copy[column])
+        return X_copy
