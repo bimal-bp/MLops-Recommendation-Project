054368d9c117792e3e15770a0abfd290ecf602d7
bimal-bp <bimalpatrap@gmail.com>
2024-02-18 00:40:54 +0530

    predictio pipeline complete

--- app.py
+++ app.py
@@ -1,4 +1,38 @@
-from src.log.logger import logging
+from fastapi import FastAPI, Form, Request, Depends, HTTPException, status
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import pandas as pd
+from pydantic import BaseModel
+from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
 
-if __name__=="__main__":
-    logging.info("logeer works fine")
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+class PredictionRequest(BaseModel):
+    user_id: str
+    product_id: str
+
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/predictdata", response_class=HTMLResponse)
+def predict_datapoint(request: Request, prediction_request: PredictionRequest = Depends()):
+    data = CustomData(
+        user_id=prediction_request.user_id,
+        product_id=prediction_request.product_id
+    )
+    pred_df = data.get_data_as_data_frame()
+    predict_pipeline = PredictPipeline()
+    results = predict_pipeline.predict(pred_df)
+    return templates.TemplateResponse("home.html", {"request": request, "results": results[0][0]})
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=80)
---
+++ src/pipeline/prediction_pipeline.py
@@ -0,0 +1,62 @@
+import sys 
+import pandas as pd 
+from src.excep.exception import CustomException
+from src.utils.common import load_object
+import os 
+import tensorflow as tf 
+import numpy as np 
+
+
+class predictpipeline:
+    def __init__(self):
+        pass 
+
+    def predict(self,features):
+        try:
+            model_path=("artifacts/model_data")
+            preprocessor_path=("artifacts/proprocessor.pkl")
+            model=tf.keras.models.load_model(model_path)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict([data_scaled[:,0],data_scaled[:,1]])
+            return preds 
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+    def recommend_products(self,model,user_id, pred, num_recommendations = 5):
+
+        all_products = np.unique(train['product_id'])
+        
+        user_input = np.array([user_id] * len(all_products))
+        product_input = all_products
+        predicted_ratings = model.predict([user_input, product_input])
+        
+        recommended_indices = np.argsort(predicted_ratings, axis=0)[-num_recommendations:][::-1]
+        
+        recommended_products = all_products[recommended_indices]
+        
+        return recommended_products
+
+        pass
+
+class CustomData:
+    def __init__(  self,
+        user_id: str,
+        product_id: str,):
+
+        self.user_id = user_id
+
+        self.product_id = product_id
+
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "user_id": [self.user_id],
+                "product_id": [self.product_id],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
